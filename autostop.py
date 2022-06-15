0abe2032fc339c8392d64e339addf2281b7bc705
Derek Phanekham <dphanekham@smu.edu>
2022-06-15 16:40:15 -0500

    switched to regex, fixed bug with not skipping, added cmdline args

--- autostop.py
+++ autostop.py
@@ -5,106 +5,107 @@ import scipy.stats as st
 import numpy
 import os
 import signal
+import argparse
+import re
 
 def live_read(command : str, interval : float, width : float, minSamples : int, numToSkip : int) -> bool:
     output_array = []
     width_achieved = False
+    current_samples = 0
     try:
         process = subprocess.Popen(shlex.split(command), shell=False, stdout=subprocess.PIPE)
         values = []
-        current_samples = 0
     except:
         print("Error: command not found")
         sys.exit(1)
     for output in process.stdout:
+        print('here')
+        if output == '':
+            continue
+        print(f'current samples {current_samples}')
         output = output.strip().decode('utf-8')
         print(output)
         output_array.append(output)
-        outputList = output.split(" ")
 
         if not width_achieved:
-            for i in range(0, len(outputList)):
-                try:
-                    if outputList[i] == 'Gbits/sec':
-                        if current_samples < numToSkip:
-                            continue
-                        else:
-                            values.append(float(outputList[i-1]))
-                            print(float(outputList[i-1]))
-                    current_samples += 1
-                except ValueError:
+
+            throughput_match = re.search(r'(?P<throughput>\d+\.?\d+?)\s+\wbits\/sec', 
+                                     output)
+            if throughput_match:
+                throughput = float(throughput_match.group('throughput'))
+                current_samples += 1
+                if current_samples <= numToSkip:
                     continue
-            if len(values) >= minSamples:
-                print(values)
-                deg_freedom = len(values) - 1
-                sample_mean = numpy.nanmean(values)
-                sample_sem = st.sem(values)
-                ci = st.t.interval(interval, deg_freedom, sample_mean, sample_sem)
-                print(ci)
-                int_width = abs(sample_mean-ci[0])
-                print("Width: " + str(int_width))
-                if int_width < width:
-                    width_achieved = True
-                    print("Target reached!")
-                    print("Final mean: " + str(sample_mean))
-                    print("Final confidence interval: " + str(ci))
-                    print("Final width: " + str(int_width))
-                    process.send_signal(signal.SIGINT)
+
+                print("using this value")
+                values.append(throughput)
+
+                if len(values) >= minSamples:
+                    print(values)
+                    deg_freedom = len(values) - 1
+                    sample_mean = numpy.nanmean(values)
+                    sample_sem = st.sem(values)
+                    ci = st.t.interval(interval, deg_freedom, sample_mean, sample_sem)
+                    print(ci)
+                    int_width = abs(sample_mean-ci[0])
+                    print("Width: " + str(int_width))
+                    if int_width < width:
+                        width_achieved = True
+                        print("Target reached!")
+                        print("Final mean: " + str(sample_mean))
+                        print("Final confidence interval: " + str(ci))
+                        print("Final width: " + str(int_width))
+                        process.send_signal(signal.SIGINT)
 
     # TODO, write this output to a file for parsing later
     print(output_array)
+    with open('iperf_output.txt', 'w') as f:
+        for line in output_array:
+            f.write(line)
+            f.write('\n')
+
     return width_achieved
 
 
-def main():
-    command = input("Enter command to execute [Default iperf -c 127.0.0.1 -t 60 -i 0.25]:")
-    if command == "":
-        command = "iperf -c 127.0.0.1 -t 60 -i 0.25"
-    # Things to ask user input: 
-    # Confidence interval percentage (95%, 90%, etc)
-    while True:
-        interval = input("Enter the desired confidence level as a decimal [Default 0.95]:")
-        if interval == "":
-            interval = 0.95
-        try:
-            interval = float(interval)
-            break
-        except ValueError:
-            print("Invalid input, please try again")
-    # Desired width and minimum # of samples
-    while True:
-        width = input("Enter target interval percentage width (+/-) [Default 2.5]:")
-        if width == "":
-            width = 2.5
-        try:
-            width = float(width)
-            break
-        except ValueError:
-            print("Invalid input, please try again")
-    while True:
-        minSamples = input("Enter minimum number of samples [Default 20]:")
-        if minSamples == "":
-            minSamples = int(10)
-        try:
-            minSamples = int(minSamples)
-            break
-        except ValueError:
-            print("Invalid input, please try again")
-    # Skip a few initial samples specified by user, default would be like 10, can be samples or seconds
-    while True:
-        numToSkip = input("Enter number of samples to skip [Default 10]:")
-        if numToSkip == "":
-            numToSkip = int(10)
-        try:
-            numToSkip = int(numToSkip)
-            break
-        except ValueError:
-            print("Invalid input, please try again")
+def main(argv):
+    # Parse command line arguments
+    command = "iperf -c 127.0.0.1 -t 60 -i 0.25 -f m"
+    level = 0.95
+    width = 2.5
+    minSamples = 10
+    numToSkip = 10
 
-    success = live_read(command, interval, width, minSamples, numToSkip)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--command', help='iPerf2 command to execute')
+    parser.add_argument('-l', '--confidence_level', help='Desired confidence level', type=float)
+    parser.add_argument('-w', '--interval_percentage_width', 
+                        help='The percentage width of the confidence interval, +-. Ex. If you give'
+                             ' a value of 2.5, this will try for an interval with a +-2.5 percent width',
+                        type=float)
+    parser.add_argument('-m', '--min_samples', help='The minimum number of samples to use', type=int)
+    parser.add_argument('-s', '--skip', help='this will skip n samples at the beginning of the test',
+                        type=int)
+
+
+    args = parser.parse_args()
+
+    print(args)
+
+    if args.command:
+        command = args.command
+    if args.confidence_level:
+        level = args.confidence_level
+    if args.interval_percentage_width:
+        width = args.interval_percentage_width
+    if args.min_samples:
+        minSamples = args.min_samples
+    if args.skip:
+        numToSkip = args.skip
+
+    success = live_read(command, level, width, minSamples, numToSkip)
     if not success:
         print("Did not attain target")
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
